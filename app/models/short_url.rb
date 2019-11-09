9096ef63292f6ffa69b359458885b3c0dea65e24
daniel <g.thirteen333@gmail.com>
2019-11-09 18:59:53 -0500

    first commit and resolved errors during dev setup

---
+++ app/models/short_url.rb
@@ -0,0 +1,20 @@
+class ShortUrl < ApplicationRecord
+
+  CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
+
+  before_save :validate_full_url
+  validates :full_url, presence: true
+
+  def short_code
+  end
+
+  def update_title!
+  end
+
+  private
+
+  def validate_full_url
+
+    return self.full_url.include?("http:") || self.full_url.include?("https:")
+  end
+end
