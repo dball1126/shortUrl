f17d25106e8bfc0afa4c47353641b380e5f03f2c
daniel <g.thirteen333@gmail.com>
2019-11-11 00:47:06 -0500

    update to short_code algo

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -8,29 +8,53 @@ class ShortUrlsController < ApplicationController
   end
 
   def index
-    @short_urls = ShortUrl.all
-    render json: @short_urls
+    
+    @short_urls = ShortUrl.limit(100)
+    @short_urls = public_attributes(@short_urls)
+    render json: {urls: @short_urls}
+  end
+
+  def update
+    @short_url = ShortUrl.find_by(id: params[:id])
+    if @short_url.update_attributes(short_url_params)
+      render json: @short_url 
+    else
+      render json: @short_url.errors.full_messages, status: 422
+    end
   end
 
   def create
     @short_url = ShortUrl.create(short_url_params)
     
-    if (!@short_url.save)
-      render json: @short_url.errors.full_messages, status: 422
-    else 
-      render json: @short_url
+    if (@short_url.save)
+      
+      render json: {title: @short_url.title, full_url: @short_url.full_url, short_code: @short_url.short_code, click_count: @short_url.click_count}
+    else
+       
+      render json: {errors: @short_url.errors.full_messages}
     end
   end
 
   def show
-    @short_url = ShortUrl.find(params[:id])
-    
-    redirect_to @short_url.full_url
+    @short_url = ShortUrl.find_by(short_code: params[:id])
+    if (@short_url)
+      @short_url.click_count += 1
+      redirect_to @short_url.full_url, format: json
+    else 
+      render json: {error: 'Url not found'}, status: :not_found
+    end
   end
 
   private
   def short_url_params
-    params.permit(:full_url, :short_code, :title)
+    params.permit(:full_url, :short_code, :title, :click_count)
   end
 
+  def public_attributes(short_urls)
+    arr = []
+    short_urls.each do |url|
+      arr << {title: url.title, short_code: url.short_code, full_url: url.full_url, click_count: url.click_count}
+    end
+    arr
+  end
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,21 +1,36 @@
 class ShortUrl < ApplicationRecord
-
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
-  before_save :validate_full_url
-  before_save :short_code
   validates :full_url, presence: true
+  validate :validate_full_url
+
+  before_save :short_code
+  before_save :update_title!
 
   def short_code
-    self.short_code = "#{id}#{CHARACTERS.sample(3)}#{id}#{CHARACTERS.sample(1)}"
+    return nil if id == nil
+    string = ""
+    length = id.to_s.length
+    main = id
+    
+    while main > 0
+      string += CHARACTERS[(main % 10) + 10]
+      main = main / 10
+    end
+    self.short_code = string + "#{CHARACTERS.sample(6 % string.length).join("")}"
+    
   end
 
   def update_title!
+    
+    self.title = title
   end
-
   private
-
   def validate_full_url
-     self.full_url.include?("http:") || self.full_url.include?("https:")
+     if(!(self.full_url.include?("http:") || self.full_url.include?("https:")))
+      errors.add(:full_url, "is not a valid url")
+      false
+     end
+      true
   end
 end
